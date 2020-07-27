56c84ef77facacebcd1e32086021a72ddae5e7cb
YDKPROD <emmanuelnteranya@gmail.com>
2020-07-27 14:16:56 +0200

    Sorting criteria inserted

--- src/main/java/org/example/taskDwh/JsonTask.java
+++ src/main/java/org/example/taskDwh/JsonTask.java
@@ -4,66 +4,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
-
-import org.javatuples.Quartet;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-/*
-public class Tuple<Kt_id, Kl_id, Pr_id, St, Kt_ts>{
-    private Kt_id kontakt_id;
-    private Kl_id klient_id;
-    private Pr_id pracownik_id;
-    private St status;
-    private Kt_ts kontakt_ts;
-}
 
-public Tuple (Kt_id kt_id, Kl_id kl_id, Pr_id pr_id, St st, Kt_ts kt_ts){
-    this.kontakt_id = kt_id;
-    this.klient_id = kl_id;
-    this.pracownik_id = pr_id;
-    this.status = st;
-    this.kontakt_ts = kt_ts;
-}
-*/
 
 public class JsonTask {
-    private static class Sorting extends ArrayList<Sorting> {
-        private Long kontakt_id;
-        private Long klient_id;
-        private Long pracownik_id;
-        private String status;
-        private Date kontakt_ts;
-
-        public Sorting(Long kontakt_id, Long klient_id, Long pracownik_id, String status, Date kontakt_ts) {
-            this.kontakt_id = kontakt_id;
-            this.klient_id = klient_id;
-            this.pracownik_id = pracownik_id;
-            this.status = status;
-            this.kontakt_ts = kontakt_ts;
-
-        }
-
-        public Long getKontakt_id() {
-            return kontakt_id;
-        }
-        public Long getKlient_id(){
-            return klient_id;
-        }
-        public Long getPracownik_id(){
-            return  pracownik_id;
-        }
-        public String getStatus(){
-            return  status;
-        }
-        private Date getKontakt_ts(){
-            return kontakt_ts;
-        }
-    }
 
 
     @SuppressWarnings("unchecked")
@@ -76,8 +24,9 @@ public class JsonTask {
         {
             //Read JSON file
             Object obj = jsonParser.parse(reader);
-
+            //Retrieve the JSON objects
             JSONObject data = (JSONObject) obj;
+            //Retrieve the array "records" only
             JSONArray data2 = (JSONArray) data.get("records");
             //Integer i = 0;
             //Empty list to append matched items
@@ -105,18 +54,16 @@ public class JsonTask {
                     Long pracownik_id = (Long) one_record.get("klient_id");
                     String  status = (String) one_record.get("status");
                     String kontakt_ts = (String) one_record.get("kontakt_ts");
-                    System.out.println(kontakt_ts);
+                    found.add(one_record);
+
                     List <Sorting>new_out = createElement(kontakt_id, klient_id, pracownik_id, status, d1);
-                    System.out.println(Arrays.asList(new Sorting(kontakt_id, klient_id, pracownik_id, status, d1)));
-                    System.out.println(createElement(kontakt_id, klient_id, pracownik_id,status, d1));
-                    System.out.println(new_out);
-
-                    //found.add(one_record);
+                    //list.set(i, (Sorting) new_out);
+                    //System.out.println(new_out);
                 }
                 }
 
 
-            //System.out.println(sorted);
+            System.out.println(found);
 
 
             //ArrayList result = found.stream().sorted(Comparator.comparing(Sorting::"Klient_id").thenComparing(Sorting::"kontakt_ts")).collect(Collectors.toList());
@@ -145,31 +92,42 @@ public class JsonTask {
     private static List<Sorting> createElement(Long klient_id, Long b, Long c, String d, Date e) {
         return Arrays.asList(new Sorting(klient_id, b, c, d, e));
     }
-    /*
-    private static void parseRecordArray (JSONArray records){
-        JSONObject data2 = (JSONObject) records.get(1);
+    private static class Sorting {
+        private Long kontakt_id;
+        private Long klient_id;
+        private Long pracownik_id;
+        private String status;
+        private Date kontakt_ts;
+
+        public Sorting(Long kontakt_id, Long klient_id, Long pracownik_id, String status, Date kontakt_ts) {
+            this.kontakt_id = kontakt_id;
+            this.klient_id = klient_id;
+            this.pracownik_id = pracownik_id;
+            this.status = status;
+            this.kontakt_ts = kontakt_ts;
+
+        }
+
+        public Long getKontakt_id() {
+            return kontakt_id;
+        }
+        public Long getKlient_id(){
+            return klient_id;
+        }
+        public Long getPracownik_id(){
+            return  pracownik_id;
+        }
+        public String getStatus(){
+            return  status;
+        }
+        public Date getKontakt_ts(){
+            return kontakt_ts;
+        }
+
+        @Override
+        public String toString(){
+            return kontakt_id + " - " + klient_id + " - " + pracownik_id + " - "+ status + " - "+ kontakt_ts;
+        }
     }
-
-     */
-    /*
-
-    private static void parseEmployeeObject(JSONObject employee)
-    {
-        //Get employee object within list
-        JSONObject employeeObject = (JSONObject) employee.get("records");
-
-        //Get employee first name
-        String firstName = (String) employeeObject.get("firstName");
-        System.out.println(firstName);
-
-        //Get employee last name
-        String lastName = (String) employeeObject.get("lastName");
-        System.out.println(lastName);
-
-        //Get employee website name
-        String website = (String) employeeObject.get("website");
-        System.out.println(website);
-    }
-    */
 
 }
